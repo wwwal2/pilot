d277fe8510d23676c421b9e7097b122cb21e44ba
wwwal2 <smalex9@gmail.com>
2019-05-23 15:17:23 +0300

    mixing components

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const element = <Fluff quallity="awesome" />
-
-function Fluff(props) {
+function MixAllElements() {
     return (
-        <h1>Fluffers {props.quallity}</h1>
+    <div>
+        <Fluff quallity="Big" />
+        <Fluff quallity="Awesome" />
+        <Fluff quallity="Fluffy" />
+    </div>
     )
 };
 
-ReactDOM.render(element, document.getElementById('root'));
+function Fluff(props) {
+    return (
+        <h1>Fluffers is {props.quallity}</h1>
+    )
+};
+
+ReactDOM.render(<MixAllElements />, document.getElementById('root'));
 
 serviceWorker.unregister();
